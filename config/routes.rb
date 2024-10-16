aa4e991e3c6f2cdbf0d64d22b3de170799149806
8kjapon <8kjapon@gmail.com>
2024-10-16 17:43:51 +0900

    update: ユーザー登録・ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "home#top"
+  resources :users, only: [:new, :create]
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
