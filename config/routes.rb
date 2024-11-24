706e94dad3786f921dc316434b7a9510ac07ec2f
8kjapon <8kjapon@gmail.com>
2024-11-25 02:27:26 +0900

    update: routes.rbの記述整形

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # ルート
   root "home#top"
+
+  # ユーザー関連
   resources :users, only: %i[new create]
   get 'mypage', to: 'users#show', as: :mypage
   get 'mypage/submit_songs', to: 'users#submit_songs', as: :submit_songs
@@ -9,6 +12,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
+  # 楽曲関連
   resources :songs, only: %i[show] do
     member do
       get 'melody', to: 'songs#melody_song_pairs'
@@ -19,10 +23,14 @@ Rails.application.routes.draw do
       get 'autocomplete'
     end
   end
+
+  # 楽曲組み合わせ関連
   resources :song_pairs, only: %i[index new create show]
   get 'recent', to: 'song_pairs#recent_page'
   get 'popular', to: 'song_pairs#popularity_page'
   resources :song_pair_evaluations, only: %i[create destroy]
+
+  # アーティスト関連
   resources :artists, only: %i[] do
     collection do
       get 'autocomplete'
