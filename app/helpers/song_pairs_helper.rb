c44ea5bc70cb8411e37d5236ff8829a9810af1c2
8kjapon <8kjapon@gmail.com>
2024-12-04 15:35:46 +0900

    update: コードの微調整

--- app/helpers/song_pairs_helper.rb
+++ app/helpers/song_pairs_helper.rb
@@ -1,15 +1,27 @@
 module SongPairsHelper
+  # 時間指定部分(12:34のような形式)を自動でウィジェットの再生時間移動リンクに変換するヘルパー
+  # `views/song_pairs/_song_info.html.erb`内で使用
   def parse_time_links(description, media_widget_id)
-    description.gsub(/(\d{1,2}:\d{2})/) do |time|
-      seconds = time_to_seconds(time)
-      link_to time, "javascript:void(0);", class: "time-link", data: { action: "media#seek", media_widget_id: media_widget_id, time: seconds }
-    end.html_safe
+    # 時間指定部分を正規表現で検知し、そこを基準に説明テキストの分割
+    # 変換が必要な部分は変換し`description_parts`に格納
+    description_parts = description.split(/(\d{1,2}:\d{2})/).map do |segment|
+      # 分割されたテキストのうち、時間指定部分であれば再生時間移動リンクに変換、そうでなければそのまま返す
+      if segment.match?(/\A\d{1,2}:\d{2}\z/)
+        seconds = time_to_seconds(segment) # 時間指定部分を秒数に変換
+        link_to(segment, "javascript:void(0);", class: "time-link", data: { action: "media#seek", media_widget_id: media_widget_id, time: seconds })
+      else
+        segment
+      end
+    end
+    # 処理の済んだ文字列を統合して出力
+    safe_join(description_parts)
   end
 
   private
 
+  # 時間指定部分(12:34のような形式)から秒数値を取得する処理
   def time_to_seconds(time_str)
-    minutes, seconds = time_str.split(":").map(&:to_i)
-    minutes * 60 + seconds
+    minutes, seconds = time_str.split(":").map(&:to_i) # `12:34`の形式を秒数計算出来るように分割
+    (minutes * 60) + seconds # 出力する秒数(計算結果)
   end
 end
--- app/javascript/controllers/media_controller.js
+++ app/javascript/controllers/media_controller.js
@@ -1,16 +1,21 @@
+// SongPairsHelperのparse_time_linksメソッドで変換された再生時間移動リンクのクリックを検知し、対応するプレイヤーウィジェットを再生する処理
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  // ウィジェットをターゲットに
   static targets = ["widget"];
 
+  // クリック時に行う処理
   seek(event) {
-    event.preventDefault();
-    const mediaWidgetId = event.currentTarget.dataset.mediaWidgetId;
-    const widgetDiv = document.getElementById(mediaWidgetId);
-    const iframe = widgetDiv ? widgetDiv.querySelector("iframe") : null;
+    const mediaWidgetId = event.currentTarget.dataset.mediaWidgetId; // ウィジェットのidを取得
+    const widgetDiv = document.getElementById(mediaWidgetId); // ウィジェット(iframe)を含む親要素divの取得
+    const iframe = widgetDiv ? widgetDiv.querySelector("iframe") : null; // ウィジェットの本体(iframe)部分の取得
     
+    // iframeが存在していれば指定の再生時間に移動するようにウィジェット設定を変更
     if (iframe) {
-      const time = parseInt(event.currentTarget.dataset.time, 10);
+      const time = parseInt(event.currentTarget.dataset.time, 10); // 指定された再生時間の秒数を10進数で取得
+
+      // ウィジェットの設定(Src部分のURLを置き換えることで変更)
       let videoSrc = iframe.src.split("?")[0];
       videoSrc += `?start=${time}&autoplay=1&enablejsapi=1`;
       iframe.src = videoSrc;
