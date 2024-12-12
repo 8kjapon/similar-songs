f314da2aa60ff4f93b74129f42caf18ed4846d0a
8kjapon <8kjapon@gmail.com>
2024-12-12 11:34:19 +0900

    update: 管理ページ用の設定

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,11 +1,13 @@
 module Admin
   class BaseController < ApplicationController
     before_action :require_admin
+    skip_before_action :set_search
+    skip_before_action :prepare_meta_tags
 
     private
 
     def require_admin
-      redirect_to root_path unless current_user&.admin?
+      redirect_to ENV["REDIRECT_PATH"], allow_other_host: true unless current_user&.admin?
     end
   end
 end
---
+++ app/controllers/admin/dashboards_controller.rb
@@ -0,0 +1,7 @@
+module Admin
+  class DashboardsController < BaseController
+    layout 'admin'
+
+    def top; end
+  end
+end
