308877099452d38832cfd58869621c31fed007b2
8kjapon <8kjapon@gmail.com>
2024-12-18 06:34:46 +0900

    fix: コード調整

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,9 @@
 class Contact < ApplicationRecord
   belongs_to :user
 
-  enum status: { pending: 0, resolved: 1 }
+  enum :status, { pending: 0, resolved: 1 }
 
-  validates :message, presence: :true
+  validates :message, presence: true
 
   # 翻訳したステータス情報を取得する処理
   def status_i18n
