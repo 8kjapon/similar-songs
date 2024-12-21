628bcd0ca1fc684bd4208b6f4a8d75ebc4de5a01
8kjapon <8kjapon@gmail.com>
2024-12-22 08:40:16 +0900

    update: 評価ボタンのAjax化

--- app/controllers/song_pair_evaluations_controller.rb
+++ app/controllers/song_pair_evaluations_controller.rb
@@ -2,12 +2,10 @@ class SongPairEvaluationsController < ApplicationController
   def create
     @song_pair = SongPair.find(params[:song_pair_id])
     current_user.song_pair_evaluation(@song_pair)
-    redirect_to @song_pair
   end
 
   def destroy
     @song_pair = current_user.song_pair_evaluations.find(params[:id]).song_pair
     current_user.song_pair_unevaluation(@song_pair)
-    redirect_to @song_pair
   end
 end
